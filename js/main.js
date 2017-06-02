8e782ccf7ecf4867b088e9b2769264efec82b8eb
RewindRL <bucketman@rewindrl.uk>
2017-06-02 11:42:32 +0100

    Fixing div scroll issues

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function main() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 70
+          scrollTop: target.offset().top - 120
         }, 1000);
         return false;
       }
