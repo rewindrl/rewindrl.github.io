717e12efc08ee7ae6e3f2ec2253256194d08d169
RewindRL <bucketman@rewindrl.uk>
2017-10-08 15:16:43 +0100

    Alterations to Twitch Banner JS

---
+++ js/twitchbanner.js
@@ -0,0 +1,5 @@
+$.get('https://api.twitch.tv/kraken/streams/rewindrl?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm', function(data) {
+	if(data.indexOf("viewers") !== -1) {
+		$(".twitch-banner").append("<p>We're streaming a tournament LIVE on <a href='http://twitch.tv/RewindRL'>Twitch</a>!</p>");
+	}
+}, 'text');
