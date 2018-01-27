a74e9f0e5e9701018a3dcf7f2e3326323e6ad1f7
RewindRL <bucketman@rewindrl.uk>
2018-01-27 17:59:40 +0000

    timezones are cool

--- js/showdown.js
+++ js/showdown.js
@@ -1,4 +1,4 @@
-if(moment().format("E") >= 2 && moment().format("E") <= 4) {
-    var matchlink = "https://smash.gg/tournament/rg-2v2-showdown-" + (parseInt(moment().format("WW")) + 14).toString() + "-eu-pc-ps4/register/embed";
+if(moment().tz("Europe/London").format("E") >= 2 && moment().tz("Europe/London").format("E") <= 4) {
+    var matchlink = "https://smash.gg/tournament/rg-2v2-showdown-" + (parseInt(moment().tz("Europe/London").format("WW")) + 14).toString() + "-eu-pc-ps4/register/embed";
     $("#signup-frame").attr("src", matchlink);
 }
--- js/weekly.js
+++ js/weekly.js
@@ -1,4 +1,4 @@
-if(moment().format("E") >= 3 && moment().format("E") <= 5) {
-    var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(moment().format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
+if(moment().tz("Europe/London").format("E") >= 3 && moment().tz("Europe/London").format("E") <= 5) {
+    var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(moment().tz("Europe/London").format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
     $("#signup-frame").attr("src", matchlink);
 }
