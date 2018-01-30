11abbc22b96ee2006c93c00b85100ac7ded9920f
RewindRL <bucketman@rewindrl.uk>
2018-01-30 08:26:51 +0000

    it was poison's fault

--- js/showdown.js
+++ js/showdown.js
@@ -1,4 +1,4 @@
 if(moment().tz("Europe/London").format("E") >= 2 && moment().tz("Europe/London").format("E") <= 4) {
-    var matchlink = "https://smash.gg/tournament/rg-2v2-showdown-" + (parseInt(moment().tz("Europe/London").format("WW")) + 14).toString() + "-eu-pc-ps4/register/embed";
+    var matchlink = "https://smash.gg/tournament/rg-2v2-showdown-" + (parseInt(moment().tz("Europe/London").format("WW")) + 13).toString() + "-eu-pc-ps4/register/embed";
     $("#signup-frame").attr("src", matchlink);
 }
