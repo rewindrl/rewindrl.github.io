07c61ee8b2abaf0b00c939689c05e4b7f77539c4
RewindRL <bucketman@rewindrl.uk>
2018-02-16 13:23:17 +0000

    k now it should work perfectly :+1:

--- js/time.js
+++ js/time.js
@@ -1,11 +1,24 @@
 function updateTime() {
     var timenow = moment().tz("Europe/London");
-    $("#livetime").html(timenow.format("dddd, H:mm:ss"));
+    $("#livetime").html(timenow.format("dddd, HH:mm:ss"));
     if (timenow.format("d") == 4) {
-        $("#countwrap").html("(That's  " + countdown(new Date(timenow.year(), timenow.month(), timenow.date(), 18, 00, 00)).toString() + " until tonight's 2v2 Showdown.)");
+        if (timenow.format("H") < 18) {
+            $("#countwrap").html("(That's  " + countdown(moment(new Date(timenow.year(), timenow.month(), timenow.date(), 18, 00, 00)).tz("Europe/London")).toString() + " until tonight's 2v2 Showdown.)");
+        }
+        else {
+            $("#countwrap").html("Tonight's 2v2 Showdown has begun!");
+        }
     }
     else if (timenow.format("d") == 5) {
-        $("#countwrap").html("(That's  " + countdown(new Date(timenow.year(), timenow.month(), timenow.date(), 18, 30, 00)).toString() + " until tonight's 3v3 Weekly.)");
+        if (timenow.format("H") <= 18 && timenow.format("m") < 30) {
+            $("#countwrap").html("(That's  " + countdown(moment(new Date(timenow.year(), timenow.month(), timenow.date(), 18, 30, 00)).tz("Europe/London")).toString() + " until tonight's 3v3 Weekly.)");
+        }
+        else {
+            $("#countwrap").html("Tonight's 3v3 Weekly has begun!");
+        }
+    }
+    else {
+        $("#countwrap").html("");
     }
     setTimeout(updateTime, 500);
 }
