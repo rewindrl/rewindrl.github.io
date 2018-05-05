e639b09a57337d20981d6b32e8811c2be24c4bee
RewindRL <bucketman@rewindrl.uk>
2018-05-05 16:33:20 +0100

    update to the new Twitch Helix API

--- js/twitchbanner.js
+++ js/twitchbanner.js
@@ -1,5 +1,14 @@
-$.get('https://api.twitch.tv/kraken/streams/RewindRL?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm', function(data) { // This is a seemingly public Twitch API key.
-	if(data.indexOf("viewers") !== -1) {
-		$("#twitch-banner").html("<p>We're streaming a tournament LIVE on <a href='http://twitch.tv/RewindRL'>Twitch</a>!</p>");
-	}
-}, 'text');
+$.ajax({
+ type: 'GET',
+ url: 'https://api.twitch.tv/helix/streams?user_id=135462003',
+ dataType: "json",
+ headers: {
+   'Client-ID': '5j0r5b7qb7kro03fvka3o8kbq262wwm' // this client ID is public as far as we know; misusing it will not affect us :)
+ },
+ success: function(data) {
+	 if(data["data"].length !== 0) {
+	 	var streamdata = data["data"][0]
+  	   $("#twitch-banner").html("<p>We're streaming the " + streamdata["title"].slice(0,20) + " LIVE on <a href='http://twitch.tv/RewindRL'>Twitch</a>!</p>");
+     }
+ }
+});
