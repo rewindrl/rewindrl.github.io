38bf2012527feecd4998e339448d565fa3b31376
RewindRL <bucketman@rewindrl.uk>
2018-05-06 14:31:30 +0100

    oof

--- js/twitchbanner.js
+++ js/twitchbanner.js
@@ -7,8 +7,8 @@ $.ajax({
  },
  success: function(data) {
 	 if(data["data"].length !== 0) {
-	 	var streamdata = data["data"][0]
-  	   $("#twitch-banner").html("<p>We're streaming the " + streamdata["title"].slice(0,17) + " LIVE on <a href='http://twitch.tv/RewindRL'>Twitch</a>!</p>");
+	    var streamdata = data["data"][0]
+  	    $("#twitch-banner").html("<p>We're streaming the " + streamdata["title"].slice(0,17) + " LIVE on <a href='http://twitch.tv/RewindRL'>Twitch</a>!</p>");
      }
  }
 });
