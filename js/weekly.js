752a38056b48defd1e7e8647409c95cfe8b60a85
RewindRL <bucketman@rewindrl.uk>
2018-05-22 23:12:17 +0100

    changes to weekly signup page

--- js/weekly.js
+++ js/weekly.js
@@ -1,7 +1,7 @@
-if(moment().tz("Europe/London").format("E") >= 3 && moment().tz("Europe/London").format("E") <= 5) {
+if(moment().tz("Europe/London").format("E") >= 2 && moment().tz("Europe/London").format("E") <= 5) {
     var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(moment().tz("Europe/London").format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
 }
 else {
     var matchlink = "/deliberatelyunwieldydocumentnametoavoidpeoplefindingthisaccidentally.html";
 }
-$("#signup-wrapper").html('<iframe id="signup-frame" src="' + matchlink + '" frameborder="0" height="100%" width="100%" vspace="0" scrolling="auto" hspace="0" allowtransparency="true"></iframe>')
+$("#signup-wrapper").html('<iframe id="signup-frame" src="' + matchlink + '" frameborder="0" height="100%" width="100%" vspace="0" scrolling="auto" hspace="0" allowtransparency="true"></iframe>');
