74381ee90aa2c4518d9ad6a036e3dd1228016bf3
RewindRL <bucketman@rewindrl.uk>
2018-05-24 17:52:55 +0100

    Improvements to signups & banner

--- js/weekly.js
+++ js/weekly.js
@@ -1,7 +1,13 @@
-if(moment().tz("Europe/London").format("E") >= 2 && moment().tz("Europe/London").format("E") <= 5) {
-    var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(moment().tz("Europe/London").format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
+function updateSignups() {
+    var timenow = moment().tz("Europe/London");
+    if(timenow.format("E") >= 2 && timenow.format("E") <= 5) {
+        var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(timenow.format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
+        $("#weeklylink").html("<p>This week's tournament page can be found <a href='https://smash.gg/RGw" + (parseInt(timenow.format("WW")) + 44).toString() + "'>here</a> if you need it.")
+    }
+    else {
+        var matchlink = "/deliberatelyunwieldydocumentnametoavoidpeoplefindingthisaccidentally.html";
+    }
+    $("#signup-wrapper").html('<iframe id="signup-frame" src="' + matchlink + '" frameborder="0" height="100%" width="100%" vspace="0" scrolling="auto" hspace="0" allowtransparency="true"></iframe>');
+    setTimeout(updateSignups, 600000);
 }
-else {
-    var matchlink = "/deliberatelyunwieldydocumentnametoavoidpeoplefindingthisaccidentally.html";
-}
-$("#signup-wrapper").html('<iframe id="signup-frame" src="' + matchlink + '" frameborder="0" height="100%" width="100%" vspace="0" scrolling="auto" hspace="0" allowtransparency="true"></iframe>');
+updateSignups();
