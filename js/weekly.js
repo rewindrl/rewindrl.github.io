fa090d2d9e89f5837552f3add77063608d167b7d
RewindRL <bucketman@rewindrl.uk>
2018-05-25 23:12:59 +0100

    cons of having an automatic weekly page generator thingy :L

--- js/weekly.js
+++ js/weekly.js
@@ -1,8 +1,8 @@
 function updateSignups() {
     var timenow = moment().tz("Europe/London");
     if(timenow.format("E") >= 2 && timenow.format("E") <= 5) {
-        var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(timenow.format("WW")) + 44).toString() + "-eu-pc-ps4/register/embed";
-        $("#weeklylink").html("<p>This week's tournament page can be found <a href='https://smash.gg/RGw" + (parseInt(timenow.format("WW")) + 44).toString() + "'>here</a> if you need it.")
+        var matchlink = "https://smash.gg/tournament/rg-3v3-weekly-" + (parseInt(timenow.format("WW")) + 43).toString() + "-eu-pc-ps4/register/embed";
+        $("#weeklylink").html("<p>This week's tournament page can be found <a href='https://smash.gg/RGw" + (parseInt(timenow.format("WW")) + 43).toString() + "'>here</a> if you need it.")
     }
     else {
         var matchlink = "/deliberatelyunwieldydocumentnametoavoidpeoplefindingthisaccidentally.html";
